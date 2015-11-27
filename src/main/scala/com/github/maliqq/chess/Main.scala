b159bc32b25acf97333f47386c6e7a3d5e306c12
Malik Baktiyarov <malik@baktiyarov.com>
2015-11-27 18:10:07 +0300

    show number of layouts

--- src/main/scala/com/github/maliqq/chess/Main.scala
+++ src/main/scala/com/github/maliqq/chess/Main.scala
@@ -6,20 +6,24 @@ object Main {
 
     var board = new Board(6, 9)
     var pieces = List[Piece](new King(), new Queen(), new Queen(), new Rook(), new Bishop(), new Knight())
+
     if (args.size == 2) {
       val Array(boardSize, piecesStr, _*) = args
       val Array(m, n) = boardSize.split('x')
       board = new Board(Integer.parseInt(m), Integer.parseInt(n))
       pieces = Pieces.parse(piecesStr).toList
     }
+
     val layout = new Layout(board)
     val boards = layout.place(pieces)
     Console printf("Solution with %s layouts (including symmetric)\n", boards.size)
-    boards.foreach { board =>
+
+    val n = 5
+    if (boards.size > n) Console printf("First %s are below:\n", n)
+    else Console println("All layouts are below:")
+
+    boards.take(n).foreach { board =>
       Console println(board.toString)
-      //Console printf("piece cells: %s \n", board.pieceNum)
-      //Console printf("aimed cells: %s \n", board.aimedNum)
-      //Console printf("free cells: %s \n", board.emptyNum)
     }
   }
 }
